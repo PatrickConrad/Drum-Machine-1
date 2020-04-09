d49a7d4a827b7357e2ebad85bf1bcf457d3707e7
MarylandGrown <paleagles93@yahoo.com>
2020-04-09 03:19:14 -0400

    second commit

--- src/components/Pages/Preview3.js
+++ src/components/Pages/Preview3.js
@@ -4,11 +4,11 @@ import DrumPad from '../DrumPad'
 const data = [
     { id: 'snare', letter: 'Q', src: 'https://www.myinstants.com/media/sounds/snare.mp3' },
     { id: 'bass 1', letter: 'W', src: 'https://www.myinstants.com/media/sounds/bass-drum.mp3' },
-    { id: 'bongo', letter: 'E', src: 'http://tipiwiki.free.fr/snd/Percussion(4e)2.wav' },
-    { id: 'tom tom', letter: 'A', src: 'http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav' },
-    { id: 'bass 3', letter: 'S', src: 'http://billor.chsh.chc.edu.tw/sound/bass4.wav' },
-    { id: 'shotgun', letter: 'D', src: 'http://david.guerrero.free.fr/Effects/ShotgunReload.wav' },
-    { id: 'high hat', letter: 'Z', src: 'http://www.denhaku.com/r_box/tr707/closed.wav' },
+    { id: 'drum roll', letter: 'E', src: 'https://www.fesliyanstudios.com/play-mp3/5744' },
+    { id: 'tom tom', letter: 'A', src: 'https://www.fesliyanstudios.com/play-mp3/6684' },
+    { id: 'cymbal', letter: 'S', src: 'https://www.fesliyanstudios.com/play-mp3/6664' },
+    { id: 'tom tom 2', letter: 'D', src: 'https://www.fesliyanstudios.com/play-mp3/6690' },
+    { id: 'high hat', letter: 'Z', src: 'https://www.fesliyanstudios.com/play-mp3/6698' },
     { id: 'drum hit', letter: 'X', src: 'http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3' },
     { id: 'laser', letter: 'C', src: 'http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav'  },
   ]
